2196cb38a4603b984651662837a8e0c1642aac8a
Nicholas Sully <nicholas.sully@digital.justice.gov.uk>
2020-11-29 14:55:24 +0000

    added time and date they called

--- src/CallBack.php
+++ src/CallBack.php
@@ -12,10 +12,20 @@ class CallBack
 
     private string $timeTheyWantACallBack;
 
-    public function __construct(string $dateForCallBack, string $timeTheyWantACallBack)
-    {
+    private string $dateOfCall;
+
+    private string $timeOfCall;
+
+    public function __construct(
+        string $dateForCallBack,
+        string $timeTheyWantACallBack,
+        string $dateOfCall,
+        string $timeOfCall
+    ) {
         $this->dateForCallBack = $dateForCallBack;
         $this->timeTheyWantACallBack = $timeTheyWantACallBack;
+        $this->dateOfCall = $dateOfCall;
+        $this->timeOfCall = $timeOfCall;
     }
 
     public function getDateForCallBack(): string
@@ -28,10 +38,21 @@ class CallBack
         return $this->timeTheyWantACallBack;
     }
 
+    public function getDateOfCall(): string
+    {
+        return $this->dateOfCall;
+    }
+
+    public function getTimeOfCall(): string
+    {
+        return $this->timeOfCall;
+    }
+
+
     public function isDateAndTimeForCallbackValid(): bool
     {
-        $todaysDateOnly = (new Carbon('now'))->toDateString();
-        $todaysTimeOnlyWithTwoHoursAdded = Carbon::parse('now')->addHours(2);
+        $todaysDateOnly = Carbon::parse($this->getDateOfCall());
+        $todaysTimeOnlyWithTwoHoursAdded = Carbon::parse($this->getTimeOfCall())->addHours(2);
         $datePassin = Carbon::parse($this->dateForCallBack);
         $timePassedIn = Carbon::parse($this->getTimeTheyWantACallBack());
         $earlierestOpeningTime = Carbon::parse('09:00:00');
@@ -56,13 +77,31 @@ class CallBack
 
         if ($todaysDateOnly === $datePassin->toDateString()) {
             if ($timePassedIn->lessThan($todaysTimeOnlyWithTwoHoursAdded)) {
+                if (($datePassin->isMonday() || $datePassin->isTuesday() || $datePassin->isWednesday())) {
+                    if ($timePassedIn->between(Carbon::parse('09:00:00'), Carbon::parse('18:00:00'))) {
+                        return true;
+                    }
+                    return false;
+                }
+                if ($datePassin->isThursday() || $datePassin->isFriday()) {
+                    if ($timePassedIn->between(Carbon::parse('09:00:00'), Carbon::parse('20:00:00'))) {
+                        return true;
+                    }
+                    return false;
+                }
+                if ($datePassin->isSaturday()) {
+                    if ($timePassedIn->between(Carbon::parse('09:00:00'), Carbon::parse('12:30:00'))) {
+                        return true;
+                    }
+                    return false;
+                }
                 return false;
             }
         } else {
             if (($datePassin->isMonday() || $datePassin->isTuesday() || $datePassin->isWednesday())) {
                 if ($timePassedIn->between(Carbon::parse('09:00:00'), Carbon::parse('18:00:00'))) {
-                   return true;
-               }
+                    return true;
+                }
                 return false;
             }
             if ($datePassin->isThursday() || $datePassin->isFriday()) {
@@ -81,5 +120,4 @@ class CallBack
 
         return true;
     }
-
 }
--- tests/CallBackTest.php
+++ tests/CallBackTest.php
@@ -9,7 +9,7 @@ class CallBackTest extends TestCase
 {
     public function testCallBackClassCanAcceptInputParameters()
     {
-        $callback = new CallBack('01/01/2015', '19:20:20');
+        $callback = new CallBack('01/01/2015', '19:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals('01/01/2015', $callback->getDateForCallBack());
         self::assertEquals('19:20:20', $callback->getTimeTheyWantACallBack());
@@ -17,56 +17,56 @@ class CallBackTest extends TestCase
 
     public function testReturnsFalseWhenDatePassedInIsBeforeTodaysDate()
     {
-        $callback = new CallBack('2020-11-28', '19:20:20');
+        $callback = new CallBack('2020-11-28', '19:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(false, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsFalseWhenTimePassedInIsNotBetweenOpeningTimes()
     {
-        $callback = new CallBack('2020-11-29', '22:20:20');
+        $callback = new CallBack('2020-11-29', '22:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(false, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsFalseDatePassInIsASunday()
     {
-        $callback = new CallBack('2020-11-29', '19:20:20');
+        $callback = new CallBack('2020-11-29', '19:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(false, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsFalseDatePassInIsMoreThenSixDaysInTheFuture()
     {
-        $callback = new CallBack('2020-12-06', '19:20:20');
+        $callback = new CallBack('2020-12-06', '19:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(false, $callback->isDateAndTimeForCallbackValid());
     }
 
-    public function testReturnsTrueDatePassInIsTheSameAsToday()
+    public function testReturnsFalseDatePassInIsTheSameAsTodayAndNotTwoHoursInTheFuture()
     {
-        $callback = new CallBack('2020-11-29', '12:20:20');
+        $callback = new CallBack('2020-11-29', '12:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(false, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsTrueDatePassInIsAMondayAndTimeIsValid()
     {
-        $callback = new CallBack('2020-11-30', '16:20:20');
+        $callback = new CallBack('2020-11-30', '16:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(true, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsTrueDatePassInIsAThursdayAndTimeIsValid()
     {
-        $callback = new CallBack('2020-12-3', '19:20:20');
+        $callback = new CallBack('2020-12-3', '19:20:20', '2020-11-29', '14:00:00');
 
         self::assertEquals(true, $callback->isDateAndTimeForCallbackValid());
     }
 
     public function testReturnsTrueDatePassInIsASundayAndTimeIsValid()
     {
-        $callback = new CallBack('2020-12-02', '12:30:00');
+        $callback = new CallBack('2020-12-02', '12:30:00', '2020-11-29', '14:00:00');
 
         self::assertEquals(true, $callback->isDateAndTimeForCallbackValid());
     }
